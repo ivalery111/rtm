fcbfe526bbf4583a3bcbd375303f28988c8d4eef
Valery <ivalery111@gmail.com>
2019-10-19 12:25:40 +0300

    Changed definitions of structs

--- src/header/definitions.h
+++ src/header/definitions.h
@@ -6,14 +6,19 @@ typedef enum{
 } OPCODE;
 
 /* Structure which contains the data */
-typedef struct message_body{
+typedef struct{
     char destination[16];
     char mask;
     char gateway_ip[16];
     char output_interface[32];
 }message_body_t;
 
-typedef struct sync_message{
+typedef struct{
     OPCODE op_code;
     message_body_t msg_body;
 }sync_message_t;
+
+/**
+ *  message_body
+ * sync_message
+ */
